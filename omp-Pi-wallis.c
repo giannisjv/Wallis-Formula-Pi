7cb386703eaaf51436dd75e6a1f5ae0301b2c2ed
giannisjv <giannisvidras@gmail.com>
2020-04-06 18:15:46 +0300

    Update omp-Pi-wallis.c

--- omp-Pi-wallis.c
+++ omp-Pi-wallis.c
@@ -1,27 +1,27 @@
-// author giannis vidras 4414002
+// author giannisjv
 #include <stdio.h>
 #include <omp.h>
 #include <math.h>
-#define cores 4
+//#define cores 4
 int main(int *argc, char **argv){
-    int N, i,c;// cores;
+    int N, i, c ,cores;
     double et, st,time[10], pi = 1.0;
-    int arxi = 100000000, telos = 1000000000 ,step = 100000000;
+    int begin = 100000000, end = 1000000000 ,step = 100000000;
     //printf("\n Give Number of loops: ");
 	//scanf("%d",&N);
 	
-	//printf("\nNumber pf cores: ");
-	//scanf("%d",&cores);
+	printf("\nNumber pf cores: ");
+	scanf("%d",&cores);
 	
 
-    for(N=arxi; N<=telos; N+=step){
+   for(N=begin; N<=end; N+=step){
     
    for(c=1;c<=cores;c++){
     
     time[i] = 0;
 	st = omp_get_wtime();
 	
-    //#pragma omp parallel for
+   
     	#pragma omp parallel for num_threads(c) reduction(*:pi)
         for (i=1; i<N; i++)
             pi *= (4.0 * i * i )/(4.0 * i * i - 1);
