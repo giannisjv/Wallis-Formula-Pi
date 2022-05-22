8a8446853736ec4984494f639ed372518cc42cb8
giannisjv <giannisvidras@gmail.com>
2022-05-22 13:23:05 +0300

    Update omp-Pi-wallis.c

--- omp-Pi-wallis.c
+++ omp-Pi-wallis.c
@@ -10,7 +10,7 @@ int main(int *argc, char **argv){
     //printf("\n Give Number of loops: ");
 	//scanf("%d",&N);
 	
-	printf("\nNumber pf cores: ");
+	printf("\nNumber of cores: ");
 	scanf("%d",&cores);
 	
 
